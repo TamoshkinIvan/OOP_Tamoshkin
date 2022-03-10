4c14295f5a8b4cd208ac7b3faa823915dbc986e5
Tamoshkin Ivan <ivantamoshkin@mail.ru>
2022-03-10 22:48:18 +0700

    solve git problem

--- LB1/ClassLibrary1/Person.cs
+++ LB1/ClassLibrary1/Person.cs
@@ -202,7 +202,7 @@ namespace ClassLibrary1
             return $"{this._name} {this._surname} {this._age} {this.Gender}";
         }
 
-        //TODO:
+        //аTODO:
         /// <summary>
         /// 
         /// </summary>
